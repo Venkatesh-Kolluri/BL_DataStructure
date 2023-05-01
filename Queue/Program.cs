da9013eda6ff10bf7e7e33c8b9b06f9db629a57a
venky <venky94.vk@gmail.com>
2023-05-01 11:18:31 +0530

    [Queue added successfully]

---
+++ Queue/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            QueueCollection<string> q = new QueueCollection<string>();
+            q.Enqueue("Welcome");
+            q.Enqueue("World");
+            q.Enqueue("Thank you");
+            q.Enqueue("Good Morning");
+            q.Print();
+
+            Console.WriteLine("The Dequeue element is: " + q.Dequeue());     //removes the first element  
+            Console.WriteLine("The Dequeue Element is " + q.Dequeue());
+            q.Enqueue("Good Bye");
+            q.Print();
+
+            Console.WriteLine("The peek element in Sting is: " + q.Peek());       //gives the first element present in the queue
+            Console.WriteLine("==============================================================================================");
+
+            QueueCollection<int> que = new QueueCollection<int>();
+            que.Enqueue(10);
+            que.Enqueue(20);
+            que.Enqueue(30);
+            que.Enqueue(40);
+
+            que.Print();
+
+            Console.WriteLine(que.Contains(20));
+
+            que.Dequeue();
+            que.Dequeue();
+
+            Console.WriteLine("The size of the queue is: " + que.Size());
+            Console.WriteLine("The peek value is: " + que.Peek());
+            Console.WriteLine("Queue in string: " + que.ToString());
+            que.Dequeue();
+
+            Console.WriteLine("The dequeue element is: " + que.Dequeue());
+
+            que.Dequeue();
+            que.Print();
+            Console.WriteLine("Is the value 30 present in the queue: " + que.Contains(30));
+        }
+    }
+}
---
+++ Queue/QueueCollection.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class QueueCollection<T>
+    {
+        int capacity;
+        T[] obj;
+        int front = -1, rear = -1;
+        public QueueCollection(int capacity=4)
+        {
+            this.capacity = capacity;
+            obj = new T[capacity];
+        }
+        public bool Enqueue(T data)
+        {
+            if (rear == capacity - 1)
+            {
+                Console.WriteLine("Queue is Full");
+                return false;
+            }
+            else if(rear==-1)
+            {
+                front++;
+
+            }
+            obj[++rear] = data;
+            return true;
+        }
+        public void Print()
+        {
+            for(int i=0; i<= rear; i++)
+            {
+                Console.WriteLine(obj[i]);
+            }
+        }      
+        public T Dequeue()
+        {
+            if(rear == - 1)
+            {
+                Console.WriteLine("Queue is Empty");
+                return default;
+
+            }
+            T ob=obj[front];
+            
+            for (int i = front; i <= rear-1; i++)
+            {
+                obj[i] = obj[i+1];
+                
+            }
+            rear=rear - 1;
+            return ob;
+
+        }
+        public T Peek()
+        {
+            T ob = obj[0];
+            return ob;             
+        }
+        public bool Contains(T data)
+        {
+            if(front == -1)
+            {
+                Console.WriteLine("Queue is Empty");
+                return false;
+            }
+            for(int i=front; i<=rear; i++)
+            {
+                if (obj[i].Equals( data))
+                {
+                    Console.WriteLine(" the queue contains: "+data);
+                    return true;              
+                }
+            }
+            return false;           
+        } 
+        public bool IsEmpty()
+        {
+            if(rear == -1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public int Size()
+        {
+            if(rear == -1)
+            {
+                return 0;
+            }
+            else
+            {
+                return rear+1;
+            }
+        }
+        public override string ToString()
+        {
+            String s = "";
+            for (int i = front; i <= rear && front != -1; i++)
+            {
+                s = s + obj[i] + " ";
+
+            }
+            return s;
+        }
+    }
+}
+
+
+
+
+
