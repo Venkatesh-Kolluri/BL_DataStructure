cc08a232b24543b14b7b21977120ecdf816a7ca0
venky <venky94.vk@gmail.com>
2023-05-01 11:19:08 +0530

    [Stack added successfully]

---
+++ Stack/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StackCollection<int> stackCollection = new StackCollection<int>();
+            stackCollection.Push(100);
+            stackCollection.Push(200);
+            stackCollection.Push(300);
+            stackCollection.Push(400);
+            //  stackCollection.Push(500);
+            stackCollection.Pop();
+
+            stackCollection.Push(500);
+            stackCollection.Print();
+            Console.WriteLine("The peek value is: " + stackCollection.Peek());
+            Console.WriteLine("The Size Of Stack is: " + stackCollection.Size());
+            stackCollection.Pop();
+
+            stackCollection.Push(9);
+
+            stackCollection.Pop();
+            stackCollection.Print();
+            stackCollection.Pop();
+            Console.WriteLine("Is Stack Empty: " + stackCollection.IsEmpty());
+        }
+    }
+}
---
+++ Stack/StackCollection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class StackCollection<T>
+    {
+        T[] obj;
+        int capacity;
+        int top = -1;
+        public StackCollection(int capacity=4)
+        {
+            this.capacity = capacity;
+            obj = new T[capacity];
+        }
+
+        public bool Push(T i)
+        {
+            if (top == capacity-1)
+            {
+                Console.WriteLine("The stack is full");
+                return false;
+            }
+            obj[++top] = i;
+            return true;
+        }
+        public T Peek()
+        {
+            if(top == -1)
+            {
+                Console.WriteLine("No value present in the stack");
+                return default;
+            }
+          
+            return obj[top];
+        }
+
+        public int Size()
+        {           
+            return (top + 1);
+        }
+
+        public void Print()
+        { 
+            for(int i = 0; i < obj.Length; i++)
+            {
+                Console.WriteLine(obj[i]);
+            }         
+        }
+        public bool IsEmpty()
+        {
+            if (top == -1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public T Pop()
+        {
+            if(top == -1)
+            {
+                Console.WriteLine("Stack is UnderFlow");
+                return default;
+            }
+
+            return obj[top--];
+        }      
+      
+
+    }
+}
